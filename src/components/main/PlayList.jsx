5678911b66cfed51d09b79b119c1f027176d2f57
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-21 17:00:16 +0900

    fetch -> axio 전환

--- src/components/main/PlayList.jsx
+++ src/components/main/PlayList.jsx
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from './VideoCard';
+import axios from 'axios';
 
 export default function PlayList() {
     const {keyword} = useParams();
     const {isLoading, error, data: videos} = useQuery(
         ['videos', keyword], async() => {
-            return fetch(`/videos/${keyword? 'search' : 'popular'}.json`)
-            .then(res => res.json())
-            .then(data => data.items);
+            return axios.get(`/videos/${keyword? 'search' : 'popular'}.json`)
+            .then(res => res.data.items);
     });
     return (
     <>
