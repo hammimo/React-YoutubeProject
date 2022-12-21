58f9a6e957e55b5cb84d6e763baa430c28d60844
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-21 14:36:51 +0900

    tailwind 적용 안됨 ( 찾아볼것)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/Header';
 function App() {
   return (
     <>
-    <Header/>
+      <Header/>
       <Outlet />
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {BiSearchAlt} from 'react-icons/bi';
 import {IoLogoYoutube} from 'react-icons/io';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom/dist';
 
-
-
 export default function Header() {
+    const {keyword} = useParams();
     const navigate = useNavigate();
     const [text, setText] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
         navigate(`/videos/${text}`)
-    }
+    };
+    useEffect(() => setText(keyword || ''), [keyword]);
     return (
         <header>  
         <Link to='/'>
-            <IoLogoYoutube/>
-            <h1>Youtube</h1>
+            <IoLogoYoutube />
+            <h1 className='text-2xl'>Youtube</h1>
         </Link>
         <form onSubmit={handleSubmit}>
             <input 
