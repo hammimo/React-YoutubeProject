b6dd24400484abb11f5abd0765eefb5dccd3e34c
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-21 16:49:17 +0900

    Mock Data 사용

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 
 function App() {
   return (
     <>
       <Header/>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/components/main/PlayList.jsx
+++ src/components/main/PlayList.jsx
@@ -1,12 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from './VideoCard';
 
 export default function PlayList() {
     const {keyword} = useParams();
+    const {isLoading, error, data: videos} = useQuery(
+        ['videos', keyword], async() => {
+            return fetch(`/videos/${keyword? 'search' : 'popular'}.json`)
+            .then(res => res.json())
+            .then(data => data.items);
+    });
     return (
-        <div>
-            videos {keyword ? `${keyword}` : '💥'}
-        </div>
+    <>
+        <div> videos {keyword ? `${keyword}` : '💥'}</div>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Something is wrong 🤣</p>}
+        {videos && <ul>
+            {videos.map((video) => (<VideoCard key ={video.id} video={video}/>))}
+            </ul>}
+    </>
     );
 }
 
