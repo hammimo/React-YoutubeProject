d4c9372d23fdf332c4147aac2f99588478dde497
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-23 04:46:44 +0900

    tailwindCSS/ index.css에서 @apply 사용

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,18 +15,19 @@ export default function Header() {
     };
     useEffect(() => setText(keyword || ''), [keyword]);
     return (
-        <header className='flex'>  
-        <Link to='/'>
-            <IoLogoYoutube />
-            <h1 className='text-2xl'>Youtube</h1>
+        <header className='flex w-full p-4 text-2xl border-b border-zinc-600 mb-4'>  
+        <Link to='/' className='flex items-center'>
+            <IoLogoYoutube className='text-4xl text-brand'/>
+            <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
         </Link>
-        <form onSubmit={handleSubmit}>
+        <form className='w-full flex justify-center' onSubmit={handleSubmit}>
             <input 
+            className='w-7/12 p-2 outline-none bg-black text-gray-50'
             type='text' 
             placeholder='Search...' 
             value={text} 
             onChange={(e) => setText(e.target.value)}/>
-            <button type='submit'><BiSearchAlt/></button>
+            <button className='bg-zinc-600 px-4'><BiSearchAlt/></button>
         </form>
         </header>
     );
