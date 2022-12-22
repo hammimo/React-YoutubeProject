ac9196680e2da25212b4597794d9dc2d97c1a2cc
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-23 04:33:31 +0900

    tailwind 고침 😊

--- src/components/api/fakeYoutube.js
+++ src/components/api/fakeYoutube.js
@@ -12,14 +12,14 @@ export default class FakeYoutube {
 
     async #searchBykeyword(){
         return axios
-            .get(`/videos/'search'.json`)
+            .get(`/videos/search.json`)
             .then(res => res.data.items)
             .then(items => items.map(item => ({...item,id:item.id.videoId})));
     }
 
     async #mostPopular(){
         return axios
-            .get(`/videos/'search'.json`)
+            .get(`/videos/popular.json`)
             .then(res => res.data.items);
     }
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import {IoLogoYoutube} from 'react-icons/io';
 import { Link, useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom/dist';
 
+
 export default function Header() {
     const {keyword} = useParams();
     const navigate = useNavigate();
@@ -14,7 +15,7 @@ export default function Header() {
     };
     useEffect(() => setText(keyword || ''), [keyword]);
     return (
-        <header>  
+        <header className='flex'>  
         <Link to='/'>
             <IoLogoYoutube />
             <h1 className='text-2xl'>Youtube</h1>
