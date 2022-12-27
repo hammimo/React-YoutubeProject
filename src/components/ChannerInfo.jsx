657be3e416121e3c1496b060318f5ba4881187c2
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-27 14:57:32 +0900

    styling

--- src/components/ChannerInfo.jsx
+++ src/components/ChannerInfo.jsx
@@ -6,9 +6,9 @@ export default function ChannerInfo({id, name}) {
     const {youtube} = useYoutubeApi();
     const {data:url} = useQuery(['channel', id], () => youtube.channelImageURL(id));
     return (
-        <div>
-            {url && <img src={url} alt={name}/>}
-            <p>{name}</p>
+        <div className='flex my-4 mb-8 items-center'>
+            {url && <img className='w-10 h-10 rounded-full' src={url} alt={name}/>}
+            <p className='text-lg font-medium ml-2'>{name}</p>
         </div>
     );
 }
